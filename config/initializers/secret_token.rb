9ef8b9945c48d990ea265ae2700e55f6a7d58cd3
memoxmrdl <jmemox@gmail.com>
2014-01-11 09:57:45 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sellfood::Application.config.secret_key_base = '5db8389b887f9e0e54ac15c41392e2b9a3b11274ae895edb033da4b8cb8613c4d8780410a197ed7562a9717352fe849b82bcccfd36e2ffca6b2770b695cff7bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sellfood::Application.config.session_store :cookie_store, key: '_sellfood_session'
