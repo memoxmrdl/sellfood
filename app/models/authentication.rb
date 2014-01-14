968f0233530e8f8c327aeac8733dcfe819e38992
memoxmrdl <jmemox@gmail.com>
2014-01-14 17:59:15 -0600

    Modificacion para la funcionalidad de omniauth con devise

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,20 @@
 class Authentication < ActiveRecord::Base
   belongs_to :user
+
+  after_create :fetch_details
+
+  def fetch_details
+    self.send("fetch_details_from_#{self.provider.downcase}")
+  end
+
+  def fetch_details_from_facebook
+    graph = Koala::Facebook::API.new(self.token)
+    facebook_data = graph.get_object("me")
+    self.username = facebook_data['username']  rescue ''
+    self.save
+    self.user.username = facebook_data['username']  rescue '' if self.user.username.blank?
+    self.user.remote_image_url = facebook_data['image'] rescue ''
+    self.user.location = facebook_data['location']  rescue '' if self.user.location.blank?
+    self.user.save
+  end
 end
