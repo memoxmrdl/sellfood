d564974522f7e7fe18460fc72a75ef644a1f4389
memoxmrdl <jmemox@gmail.com>
2014-01-15 13:18:51 -0600

    refactor and fix model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,39 +2,40 @@ class User < ActiveRecord::Base
   has_one :authentication
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable#, :token_authenticatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
-  #attr_accessible :email, :password, :password_confirmation
-
-  def self.new_with_session(params, session)
-    if session["devise.user_attributes"]
-      new(session["devise.user_attributes"], without_protection: true) do |user|
-        user.attributes = params
-        user.valid?
+  class << self
+    def new_with_session(params, session)
+      if session["devise.user_attributes"]
+        new(session["devise.user_attributes"], without_protection: true) do |user|
+          user.attributes = params
+          user.valid?
+        end
+      else
+        super
       end
-    else
-      super
     end
-  end
 
-  def self.from_omniauth(auth, current_user)
-    authorization = Authorization.where(:provider => auth.provider, 
-                                        :uid => auth.uid.to_s, 
-                                        :token => auth.credentials.token, 
-                                        :secret => auth.credentials.secret).first_or_initialize
-    if authorization.user.blank?
-      user = current_user.nil? ? User.where('email = ?', auth["info"]["email"]).first : current_user
-      if user.blank?
-       user = User.new
-       user.password = Devise.friendly_token[0,10]
-       user.name = auth.info.name
-       user.email = auth.info.email
-       auth.provider == "facebook" ?  user.save(:validate => false) :  user.save
-     end
-     authorization.username = auth.info.nickname
-     authorization.user_id = user.id
-     authorization.save
+    def from_omniauth(auth, current_user)
+      authorization = Authentication.where(:provider => auth.provider, 
+                                          :uid => auth.uid.to_s, 
+                                          :token => auth.credentials.token, 
+                                          :secret => auth.credentials.secret).first_or_initialize
+      if authorization.user.blank?
+        user = current_user.nil? ? User.where(:email => auth["info"]["email"]).first : current_user
+        if user.blank?
+          user = User.new do |user|
+            user.password = Devise.friendly_token[0, 10]
+            user.name = auth.info.name 
+            user.email = auth.info.email 
+            auth.provider == "facebook" ? user.save(:validate => false) : user.save
+          end
+       end
+        authorization.username = auth.info.nickname
+        authorization.user_id = user.id
+        authorization.save
+      end
+      authorization.user
     end
-    authorization.user
   end
 end
