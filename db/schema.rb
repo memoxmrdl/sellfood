f47474986c19d8aa3905f1d77017b8a277b1ede3
memoxmrdl <jmemox@gmail.com>
2014-01-14 18:01:33 -0600

    Schema actualizado

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140114194212) do
+ActiveRecord::Schema.define(version: 20140114233911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140114194212) do
     t.string   "uid",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   create_table "users", force: true do |t|
@@ -37,6 +38,8 @@ ActiveRecord::Schema.define(version: 20140114194212) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "userame"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
