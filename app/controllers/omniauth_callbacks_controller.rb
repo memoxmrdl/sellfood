204e004e079af4657a0cd25662a77ece5fcc7b56
memoxmrdl <jmemox@gmail.com>
2014-01-14 18:00:11 -0600

    Agregando controlador callback de omniauth

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,22 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_filter :authenticate_user!
+  
+  def all
+    p env["omniauth.auth"]
+    user = User.from_omniauth(env["omniauth.auth"], current_user)
+
+    if user.persisted?
+      flash[:notice] = "You are in..!!! Go to edit profile to see the status for the accounts"
+      sign_in_and_redirect(user)
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    super
+  end
+
+  alias_method :facebook, :all
+end
