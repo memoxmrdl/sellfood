838241d5d2df36437812ecc3d5ca1f942eacebf1
memoxmrdl <jmemox@gmail.com>
2014-01-15 13:42:51 -0600

    refactor code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Sellfood::Application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  devise_for :users, 
+             :path => '', 
+             :controllers => { omniauth_callbacks: "omniauth_callbacks" }
 
   root 'home#index'
 end
