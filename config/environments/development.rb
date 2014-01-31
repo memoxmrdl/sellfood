19459fffeae19a1040671a73d0e22fa071e1a126
memoxmrdl <jmemox@gmail.com>
2014-01-31 14:29:05 -0600

    changes in devise wiews

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,17 @@ Sellfood::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # Configuration settings email  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'sellfood.com',
+    user_name:            'sellfood24@gmail.com',
+    password:             '2CinturondeOrion4',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
