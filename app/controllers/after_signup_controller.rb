595f29315d57ea7cbddfc5c41dce13439db14f6d
memoxmrdl <jmemox@gmail.com>
2014-02-11 00:40:10 -0600

    changes wizar in show

--- app/controllers/after_signup_controller.rb
+++ app/controllers/after_signup_controller.rb
@@ -4,6 +4,18 @@ class AfterSignupController < ApplicationController
   steps :setup, :billing
 
   def show
+    @user = current_user
+
+    case step
+    when :setup
+      @account = Account.find_or_initialize_by_user_id(@user.id)
+    end
+
+    case step
+    when :billing
+    end
+
+    render_wizard 
   end
 
   def update
